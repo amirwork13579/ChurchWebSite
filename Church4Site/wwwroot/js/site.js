163b0d5dbef498e984bca20c83dc7379ac4b9d00
4rima <4rima@DESKTOP-6OLG9B2>
2025-12-23 20:06:00 +0000

    added the hamburger js

--- Church4Site/wwwroot/js/site.js
+++ Church4Site/wwwroot/js/site.js
@@ -2,3 +2,29 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+
+/*hamburger menu js------------*/
+document.addEventListener('DOMContentLoaded', () => {
+    const trigger = document.querySelector('#portal-toggle');
+    const wrapper = document.querySelector('.portal-wrapper');
+
+    if (trigger && wrapper) {
+        trigger.addEventListener('click', () => {
+            // Toggles the hamburger icon state
+            trigger.classList.toggle('is-open');
+            // Toggles the visibility of the menu overlay
+            wrapper.classList.toggle('is-visible');
+        });
+
+        // Close menu if a user clicks a link (helpful for one-page sites)
+        wrapper.querySelectorAll('a').forEach(item => {
+            item.addEventListener('click', () => {
+                trigger.classList.remove('is-open');
+                wrapper.classList.remove('is-visible');
+            });
+        });
+    }
+});
